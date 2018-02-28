e8ae545a13e44dd25ead6f4ced35db3ad3f9579d
zb2oby <zbooby@dev.famille>
2018-02-28 21:10:47 +0100

    visiteur : redirection en cas de pas d'expo

---
+++ visiteur/error.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>No Expo</title>
+        <link rel="stylesheet" href="style.css">
+
+    </head>
+    <body>
+        <div class="message" style="font-size: 16px;">
+			Ooups Il n'y à pas d'expo en ce moment
+		</div> 
+    </body>
+</html>
+
+	
+
